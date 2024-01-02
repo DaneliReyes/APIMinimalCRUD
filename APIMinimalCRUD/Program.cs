feafd89c17bed7c267e96c133cbf656fa496a5b8
daneli.reyes <daneli.reyes@proan.com>
2024-01-02 11:39:31 -0600

    Configure swagger

--- APIMinimalCRUD/Program.cs
+++ APIMinimalCRUD/Program.cs
@@ -1,5 +1,19 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ejemplo minimal API", Description = "Codigo de ejemplo" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
